d4235f795ecffac5f05a1f42d2e2883e63645431
Juan Estrella <juan.estrella137@gmail.com>
2020-07-10 20:49:13 +0200

    Use Axios to send GET request to youtube API

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,32 @@
 import React from 'react';
 import SearchBar from './SearchBar';
+import youtube from '../apis/youtube';
+import VideoList from './VideoList';
+
+const KEY = 'AIzaSyAdAH5aZgO0nTQctqNO_leQzgvFDHu6YW0';
 
 class App extends React.Component {
+  state = { videos: [] };
+
+  onTermSubmit = async (term) => {
+    const response = await youtube.get('/search', {
+      params : {
+        q          : term,
+        part       : 'snippet',
+        type       : 'video',
+        maxResults : 5,
+        key        : `${KEY}`
+      }
+    });
+
+    this.setState({ videos: response.data.items });
+  };
+
   render() {
     return (
       <div className="ui container">
-        <SearchBar />
+        <SearchBar onFormSubmit={this.onTermSubmit} />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
