1e352c887b9c649f30151193fac6ffc188fc59fb
Juan Estrella <juan.estrella137@gmail.com>
2020-07-11 18:35:40 +0200

    Set state of App component on search submit

--- src/components/App.js
+++ src/components/App.js
@@ -5,10 +5,13 @@ import VideoList from './VideoList';
 import VideoDetail from './VideoDetail';
 
 const KEY = 'AIzaSyAdAH5aZgO0nTQctqNO_leQzgvFDHu6YW0';
-const EKEY = `${process.env.REACT_APP_YOUTUBE_API_KEY}`;
 
 class App extends React.Component {
   state = { videos: [], selectedVideo: null };
+
+  componentDidMount() {
+    this.onTermSubmit('Lionel Messi');
+  }
 
   onTermSubmit = async (term) => {
     const response = await youtube.get('/search', {
@@ -21,7 +24,10 @@ class App extends React.Component {
       }
     });
 
-    this.setState({ videos: response.data.items });
+    this.setState({
+      videos        : response.data.items,
+      selectedVideo : response.data.items[0]
+    });
   };
 
   onVideoSelect = (video) => {
